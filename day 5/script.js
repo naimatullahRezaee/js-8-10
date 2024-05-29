63010ad36654120965ae9994286868f2b07b33a9
naimatullahRezaee <naimatullahrezaee@gmail.com>
2024-05-29 09:51:09 +0430

    complete

---
+++ day 5/script.js
@@ -0,0 +1,41 @@
+// let x = 12;
+// let y = 100;
+
+// console.log(x / y);
+
+// var a = 2;
+// var b = 10;
+
+// console.log(a ** b);
+
+// let count = 0;
+
+// console.log(--count);
+// console.log(count);
+
+// let x = 90;
+
+// let y = 100;
+
+// x = y;
+
+// console.log(x);
+
+// let x = 30;
+// let y = 30;
+
+// console.log(x !== y);
+
+// let a = false;
+
+// console.log(!a);
+
+let r = true;
+
+// r ? console.log("Hello world!") : console.log("Goodbye");
+
+if (r) {
+  console.log("Hello world!");
+} else {
+  console.log("Goodbye");
+}
