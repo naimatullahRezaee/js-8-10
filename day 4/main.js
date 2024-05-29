f1623f96e1fd423644e7550089e7839a6baf88aa
naimatullahRezaee <naimatullahrezaee@gmail.com>
2024-05-29 08:33:54 +0430

    day 5

---
+++ day 4/main.js
@@ -0,0 +1,67 @@
+// let first_name = "marwa";
+// let lastname = "ahmadi";
+
+// marwa ahmadi
+
+// console.log(first_name);
+// console.log(typeof first_name);
+
+// console.log(
+//   "your firstname is : " + first_name + "  and your lastname is: " + lastname
+// );
+
+// let first_name = "marwa";
+
+// let x = 20;
+// let y = 10;
+
+// console.log(x + y + first_name);
+
+// let num = 20084379837598739857398759837857n;
+
+// console.log(num);
+// console.log(typeof num);
+
+// let x = true;
+// let y = false;
+
+// var a = 50 < 10;
+
+// console.log(a);
+
+// null , undefined
+
+// let x;
+
+// console.log(x);
+// console.log(typeof x);
+
+// let y = null;
+// console.log(y);
+// console.log(typeof y);
+
+// console.log(undefined == null);
+
+// Reference datatype
+
+// let num = [1, 2, 3, 4, 5];
+
+// console.log(num);
+// console.log(typeof num);
+
+// object
+
+// var students = {
+//   name: "marwa",
+//   age: 20,
+//   gender: "female",
+// };
+
+// console.log(students);
+// console.log(typeof students);
+
+function msg() {
+  console.log("Hello");
+}
+
+console.log(typeof msg);
